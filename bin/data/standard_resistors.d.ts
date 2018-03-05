228c4ad3d267bacc7c624583ca1ac3f7f4ba43a5
Jordan Baczuk <Jordan@kilfetech.com>
2018-03-05 09:59:37 -0700

    remove bin folder

--- bin/data/standard_resistors.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ResistorSet {
-    tolerance: string;
-    resistances: number[];
-    constructor(tolerance: string, resistances: number[]);
-    populate_resistor_values(values: number[]): number[];
-}
-declare const _default: ResistorSet[];
-export default _default;
