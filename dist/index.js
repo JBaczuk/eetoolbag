20b99654912e496d26008c435250c9f64ebe8055
Jordan Baczuk <Jordan@kilfetech.com>
2018-03-05 09:18:13 -0700

    remove build and node_modules folders and rename for ee cli command

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var standard_resistors = require("./data/standard_resistors");
-standard_resistors.default.forEach(function (set) {
-    if (set.tolerance == 'E12') {
-        console.log(set);
-    }
-});
